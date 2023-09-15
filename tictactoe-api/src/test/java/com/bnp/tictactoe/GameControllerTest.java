854f9fa90e352275507a5592ffd8ccfac7916cf5
Muhammad Amar <muhammadamarbhoon@gmail.com>
2023-09-15 12:45:15 +0500

    Add more unit test to cover uncovered scenarios

--- tictactoe-api/src/test/java/com/bnp/tictactoe/GameControllerTest.java
+++ tictactoe-api/src/test/java/com/bnp/tictactoe/GameControllerTest.java
@@ -35,7 +35,7 @@ public class GameControllerTest {
     private ObjectMapper objectMapper;
 
     @BeforeEach
-    public void initMocks() throws Exception {
+    public void init() {
         GameContextHolder GAME_CONTEXT = GameContextHolder.getInstance();
         Game existingGame = new Game();
         existingGame.setGameId(UUID.fromString("5e8bac93-fbfd-46de-85be-58b7ddb30efa"));
@@ -88,18 +88,38 @@ public class GameControllerTest {
     }
 
     @Test
-    void testPerformTurn() throws Exception {
-        createNewGameState();
+    void testPerformTurnDraw() throws Exception {
+        createNewGameState("0ce3758e-5341-11ee-8c99-0242ac120002");
         testPerformFirstTurnXShouldAlwaysGoFirst();
-        testPerformTurnSuccess();
+        testPerformTurnSuccess(1,1,"X","0ce3758e-5341-11ee-8c99-0242ac120002");
         testPerformTurnCannotPlayOnPlayedPosition();
         testPerformTurnAlternateTurn();
+        testPerformTurnSuccess(2,2,"O", "0ce3758e-5341-11ee-8c99-0242ac120002");
+        testPerformTurnSuccess(2,1,"X","0ce3758e-5341-11ee-8c99-0242ac120002");
+        testPerformTurnSuccess(3,1,"O", "0ce3758e-5341-11ee-8c99-0242ac120002");
+        testPerformTurnSuccess(1,3,"X", "0ce3758e-5341-11ee-8c99-0242ac120002");
+        testPerformTurnSuccess(1,2,"O", "0ce3758e-5341-11ee-8c99-0242ac120002");
+        testPerformTurnSuccess(3,2,"X","0ce3758e-5341-11ee-8c99-0242ac120002");
+        testPerformTurnSuccess(2,3,"O","0ce3758e-5341-11ee-8c99-0242ac120002");
+        testPerformTurnSuccess(3,3,"X","0ce3758e-5341-11ee-8c99-0242ac120002");
+        testGetGameStateInAGameDraw();
     }
 
-    public void createNewGameState() {
+    @Test
+    void testPerformTurnWinnerX() throws Exception {
+        createNewGameState("0ce3758e-5341-11ee-8c99-0242ac120003");
+        testPerformTurnSuccess(1,1,"X", "0ce3758e-5341-11ee-8c99-0242ac120003");
+        testPerformTurnSuccess(2,2,"O", "0ce3758e-5341-11ee-8c99-0242ac120003");
+        testPerformTurnSuccess(2,1,"X", "0ce3758e-5341-11ee-8c99-0242ac120003");
+        testPerformTurnSuccess(3,2,"O", "0ce3758e-5341-11ee-8c99-0242ac120003");
+        testPerformTurnSuccess(3,1,"X", "0ce3758e-5341-11ee-8c99-0242ac120003");
+        testGetGameStateInAWinnerX();
+    }
+
+    public void createNewGameState(String gameId) {
         GameContextHolder GAME_CONTEXT = GameContextHolder.getInstance();
         Game existingGame = new Game();
-        existingGame.setGameId(UUID.fromString("0ce3758e-5341-11ee-8c99-0242ac120002"));
+        existingGame.setGameId(UUID.fromString(gameId));
         existingGame.setStatus(GameStatusEnum.STARTED);
         existingGame.setWinner(null);
         existingGame.setBoard(new int[][]{{0,0,0},
@@ -120,9 +140,9 @@ public class GameControllerTest {
                         .value("X turn should always goes first."));
     }
 
-    void testPerformTurnSuccess() throws Exception {
-        mockMvc.perform(patch("/api/v1/turn/0ce3758e-5341-11ee-8c99-0242ac120002")
-                        .content(objectMapper.writeValueAsString(getTurnRequest(1,1, "X")))
+    void testPerformTurnSuccess(int rowIndex, int colIndex, String playerId, String gameId) throws Exception {
+        mockMvc.perform(patch("/api/v1/turn/" + gameId)
+                        .content(objectMapper.writeValueAsString(getTurnRequest(rowIndex,colIndex, playerId)))
                         .contentType(MediaType.APPLICATION_JSON))
                 .andDo(print()).andExpect(status().isNoContent());
     }
@@ -147,6 +167,48 @@ public class GameControllerTest {
                 .andExpect(jsonPath("$.code").value("TTT-10"))
                 .andExpect(jsonPath("$.message")
                         .value("Players must take alternate turn."));
+    }
+
+    public void testGetGameStateInAGameDraw() throws Exception {
+        mockMvc.perform(get("/api/v1/state/0ce3758e-5341-11ee-8c99-0242ac120002"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.gameId").value("0ce3758e-5341-11ee-8c99-0242ac120002"))
+                .andExpect(jsonPath("$.status").value("Finished"))
+                .andExpect(jsonPath("$.winner").value("DRAW"))
+                .andExpect(jsonPath("$.board.positionInfoList[0].playerId").value("X"))
+                .andExpect(jsonPath("$.board.positionInfoList[1].playerId").value("O"))
+                .andExpect(jsonPath("$.board.positionInfoList[2].playerId").value("X"))
+
+
+                .andExpect(jsonPath("$.board.positionInfoList[3].playerId").value("X"))
+                .andExpect(jsonPath("$.board.positionInfoList[4].playerId").value("O"))
+                .andExpect(jsonPath("$.board.positionInfoList[5].playerId").value("O"))
+
+                .andExpect(jsonPath("$.board.positionInfoList[6].playerId").value("O"))
+                .andExpect(jsonPath("$.board.positionInfoList[7].playerId").value("X"))
+                .andExpect(jsonPath("$.board.positionInfoList[8].playerId").value("X"));
+    }
+
+    public void testGetGameStateInAWinnerX() throws Exception {
+        mockMvc.perform(get("/api/v1/state/0ce3758e-5341-11ee-8c99-0242ac120003"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.gameId").value("0ce3758e-5341-11ee-8c99-0242ac120003"))
+                .andExpect(jsonPath("$.status").value("Finished"))
+                .andExpect(jsonPath("$.winner").value("X"))
+                .andExpect(jsonPath("$.board.positionInfoList[0].playerId").value("X"))
+                .andExpect(jsonPath("$.board.positionInfoList[1].playerId").value("-"))
+                .andExpect(jsonPath("$.board.positionInfoList[2].playerId").value("-"))
+
+
+                .andExpect(jsonPath("$.board.positionInfoList[3].playerId").value("X"))
+                .andExpect(jsonPath("$.board.positionInfoList[4].playerId").value("O"))
+                .andExpect(jsonPath("$.board.positionInfoList[5].playerId").value("-"))
+
+                .andExpect(jsonPath("$.board.positionInfoList[6].playerId").value("X"))
+                .andExpect(jsonPath("$.board.positionInfoList[7].playerId").value("O"))
+                .andExpect(jsonPath("$.board.positionInfoList[8].playerId").value("-"));
     }
 
 
